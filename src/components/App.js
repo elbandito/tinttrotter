f2c37203e3d08ccd2b5f344f9a8c8d5dca47602d
Travis Longoria <longoria.public@gmail.com>
2017-08-21 23:00:25 -0600

    semantic-ui

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 import TodoList from './TodoList';
 import AddTodoItem from '../containers/AddTodoItem';
+import {Button} from 'semantic-ui-react';
 
 const App = () => {
     return (
         <div>
             <h1>Tint!</h1>
+            <div>
+                <Button primary>Primary</Button>
+                <Button secondary>Secondary</Button>
+            </div>
             <AddTodoItem />
             <TodoList />
         </div>
