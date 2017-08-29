e1ef410b31d041d30b211044661d3052e18009c0
Travis Longoria <longoria.public@gmail.com>
2017-08-28 22:46:03 -0600

    Try to fix cors issue

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const allowCrossDomain = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(request, response) {
